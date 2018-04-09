445abbe057fc9c5001d3cfd857486a57d798b868
Falcon43 <aishwarya.rk347@gmail.com>
2018-04-09 07:32:38 +0530

    Update timeit_function.py

--- SCCI/timeit_function.py
+++ SCCI/timeit_function.py
@@ -9,6 +9,9 @@ for i in range(1,41):
     time1 = Timer(s,"from fibonacci import fibo").timeit(3)
     s = "fibo_iterative("+str(i)+")"
     time2 = Timer(s,"from fibonacci import fibo_iterative").timeit(3)
-    print("n=%2d, fibo: %8.6f , fibo_iterative: %7.6f, percent:%10.2f" % (i,time1,time2,time1/time2))
+    s = "fibo_memory("+str(i)+")"
+    time3 = Timer(s,"from fibonacci import fibo_memory").timeit(3)
+    print("n=%2d, fibo: %8.6f , fibo_iterative: %7.6f, fibo_memo: %7.6f, percent:%10.2f" % (i,time1,time2,time3,time1/time2))
+
 
 
