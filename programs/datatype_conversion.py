aa9602a65dbe503f52a72976b7f66dc95756f5ea
Falcon43 <aishwarya.rk347@gmail.com>
2019-03-14 14:03:24 +0530

    Add files via upload

--- programs/datatype_conversion.py
+++ programs/datatype_conversion.py
@@ -5,3 +5,22 @@ print("int() of 3.8 : ", int(3.8) , "\n")
 print("int() of -3.2 : ", int(-3.2) , "\n")
 print("int() of -3.5 : ", int(-3.5) , "\n")
 print("int() of -3.8 : ", int(-3.8) , "\n")
+
+
+
+"""
+int() acts like   math.floor(x)  or  ⌊𝑥⌋  
+
+int() of 3.2 :  3 
+
+int() of 3.5 :  3 
+
+int() of 3.8 :  3 
+
+int() of -3.2 :  -3 
+
+int() of -3.5 :  -3 
+
+int() of -3.8 :  -3 
+
+"""
