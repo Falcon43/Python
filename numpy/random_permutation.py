3950a3534c449c718863228f5dcf3534553e91f8
Falcon43 <aishwarya.rk347@gmail.com>
2019-03-15 15:18:59 +0530

    Add files via upload

--- numpy/random_permutation.py
+++ numpy/random_permutation.py
@@ -14,3 +14,21 @@ shuffled_X = X[:,p]
 shuffled_Y = Y[:,p]
 print("Shuffled X: " ,shuffled_X)
 print("Shuffled Y: " ,shuffled_Y)
+
+
+"""
+Output:
+
+X:  [[6 8 7 5 4]
+ [8 4 5 7 4]
+ [8 4 6 3 7]]
+Y:  [[8 5 3 5 3]]
+
+permutation [0 3 4 2 1] 
+
+Shuffled X:  [[6 5 4 7 8]
+ [8 7 4 5 4]
+ [8 3 7 6 4]]
+Shuffled Y:  [[8 5 3 3 5]]
+
+"""
